4b82356dae11a315452ac523cdc6e7c175b7a71a
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-07 14:18:49 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,21 +10,36 @@ Hefter
 
 let unserLager = {
     schreibtisch: {
-      schublade: "Hefter"
+        schublade: "Hefter"
     },
     schrank: {
-      "Obere Schublade": {
-        Ordner1: "Dokumente",
-        Ordner2: "Geheimnisse"
-      },
-      "Untere Schublade": "Cola"
+        "Obere Schublade": {
+            Ordner1: "Dokumente",
+            Ordner2: "Geheimnisse"
+        },
+        "Untere Schublade": "Cola"
     }
-  };
-  
+};
 
-  console.log(unserLager)
-  console.table(unserLager)
-  
-  // ausgabe Geheimnisse
-  console.log(unserLager.schrank["Obere Schublade"].Ordner2)
+
+console.log(unserLager)
+console.table(unserLager)
+
+// ausgabe Geheimnisse
+console.log(unserLager.schrank["Obere Schublade"].Ordner2)
 console.log(unserLager["schrank"]["Obere Schublade"]["Ordner2"])
+
+// ausgabe Cola
+console.log(unserLager.schrank["Untere Schublade"]);
+console.log(unserLager["schrank"]["Untere Schublade"])
+
+// in String hinzufügen +=     ersetzen = 
+unserLager["schrank"]["Untere Schublade"] += " Spezi"
+console.log(unserLager["schrank"]["Untere Schublade"])
+console.log(unserLager)
+console.table(unserLager)
+
+
+// ausgabe Hefter
+console.log(unserLager.schreibtisch.schublade)
+console.log(unserLager["schreibtisch"]["schublade"])
