8ce3554ed9aa24fde94ec54e663e06562f750033
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-07 14:07:52 +0100

    testGeheimniss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
 console.log("test");
+
+/* 
+Geheimnisse
+Cola
+Hefter
+*/
+
+
+
+let unserLager = {
+    schreibtisch: {
+      schublade: "Hefter"
+    },
+    schrank: {
+      "Obere Schublade": {
+        Ordner1: "Dokumente",
+        Ordner2: "Geheimnisse"
+      },
+      "Untere Schublade": "Cola"
+    }
+  };
+  
+
+  console.log(unserLager)
+  console.table(unserLager)
+  
+  // ausgabe Geheimnisse
+  console.log(unserLager.schrank["Obere Schublade"].Ordner2)
+console.log(unserLager["schrank"]["Obere Schublade"]["Ordner2"])
